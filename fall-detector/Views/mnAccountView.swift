04b2e9570ab7998fc82b003d86fc168ae820a35d
Harry Wixley <hwixley1@gmail.com>
2022-01-14 17:53:41 +0000

    adding data to accountView

--- fall-detector/Views/mnAccountView.swift
+++ fall-detector/Views/mnAccountView.swift
@@ -15,7 +15,36 @@ struct mnAccountView: View {
             ZStack(alignment: .top) {
                 MyColours.g0.edgesIgnoringSafeArea(.all)
                 VStack(spacing:40) {
-
+                    if self.appState.inappState.user != nil {
+                        Text("Personal Information:")
+                        
+                        Divider()
+                        
+                        Text("Name: \(appState.inappState.user!.info.name)")
+                        Text("Phone number: \(appState.inappState.user!.info.phone)")
+                        Text("Email: \(appState.inappState.user!.info.email)")
+                        
+                        Divider()
+                        
+                        Text("Emergency Contacts:")
+                        
+                        Divider()
+                        
+                        let numContacts = appState.inappState.user!.contacts.count
+                        ForEach(0..<numContacts) { i in
+                            let contact = appState.inappState.user!.contacts[i]
+                            
+                            Divider()
+                            
+                            Text("Contact \(i+1):")
+                            
+                            Divider()
+                            
+                            Text("Name: \(contact.name)")
+                            Text("Phone number: \(contact.phone)")
+                            Text("Email: \(contact.email)")
+                        }
+                    }
                 }
                 .modifier(NavigationBarStyle(title: "Account", page: .main, hideBackButton: false, appState: appState))
             }
