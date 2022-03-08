d5b13676009941a6a4843d154ec9aa284fb86738
Harry Wixley <hwixley1@gmail.com>
2022-03-08 13:02:39 +0000

    setup stats page

--- fall-detector/Views/StatsView.swift
+++ fall-detector/Views/StatsView.swift
@@ -15,13 +15,41 @@ struct StatsView: View {
         NavigationView {
             ZStack(alignment: .top) {
                 MyColours.g0.edgesIgnoringSafeArea(.all)
-                VStack(spacing:40) {
+                VStack {
+                    VStack {
+                        VStack {
+                            Text("BIOMETRICS")
+                                .modifier(DefaultText(size: 20))
+                            
+                            Divider()
+                            
+                            CustLabel(title: "Average heart rate", value: " BPM")
+                        }
+                        .padding(.all, 10)
+                        .background(MyColours.b1)
+                    }
+                    .cornerRadius(20)
                     
-
+                    VStack {
+                        VStack {
+                            Text("FALL DETECTION")
+                                .modifier(DefaultText(size: 20))
+                            
+                            Divider()
+                            
+                            CustLabel(title: "# Falls detected:", value: "")
+                            CustLabel(title: "TPR:", value: "%")
+                            CustLabel(title: "FPR:", value: "%")
+                        }
+                        .padding(.all, 10)
+                        .background(MyColours.b1)
+                    }
+                    .cornerRadius(20)
                 }
-                .modifier(NavigationBarStyle(title: "Stats", page: .entry, hideBackButton: true, appState: appState))
+                .padding(.all, 10)
             }
             .modifier(BackgroundStack(appState: appState, backPage: nil))
+            .modifier(NavigationBarStyle(title: "Stats", page: .entry, hideBackButton: true, appState: appState))
         }
         .navigationViewStyle(StackNavigationViewStyle())
     }
